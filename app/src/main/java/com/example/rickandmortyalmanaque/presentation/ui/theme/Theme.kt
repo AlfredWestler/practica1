c27be5d0ce69765e9d454a8a57df3a8a87fcfcfc
ALFREDO SALVADOR GRANCIANO HERNANDEZ <alfredo.granciano@elektra.com.mx>
2023-03-23 17:31:11 -0600

    navigation graph

--- app/src/main/java/com/example/rickandmortyalmanaque/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/rickandmortyalmanaque/presentation/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ fun RickAndMortyAlmanaqueTheme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }

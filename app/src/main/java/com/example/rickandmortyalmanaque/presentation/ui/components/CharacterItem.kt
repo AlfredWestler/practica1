d8a8bef0cb9aff87d15d918b50c2704cda7910e9
ROBERTO ANTONIO GUTIERREZ CAMPOS <roberto.gutierrez@elektra.com.mx>
2023-03-23 18:43:27 -0600

    fixes

--- app/src/main/java/com/example/rickandmortyalmanaque/presentation/ui/components/CharacterItem.kt
+++ app/src/main/java/com/example/rickandmortyalmanaque/presentation/ui/components/CharacterItem.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -46,6 +48,8 @@ fun CharacterItem(
             modifier = Modifier
                 .align(Alignment.BottomStart)
                 .padding(10.dp)
+                .background(Color.White)
+                .clip(CircleShape)
         )
     }
 }
--- app/src/main/java/com/example/rickandmortyalmanaque/presentation/ui/components/CharactersScreen.kt
+++ app/src/main/java/com/example/rickandmortyalmanaque/presentation/ui/components/CharactersScreen.kt
@@ -2,15 +2,21 @@ package com.example.rickandmortyalmanaque.presentation.ui.components
 
 import android.annotation.SuppressLint
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.rickandmortyalmanaque.CharacterItem
+import com.example.rickandmortyalmanaque.presentation.ui.components.shimmer.GSBCShimmerAnimation
 import com.example.rickandmortyalmanaque.ui.MortyModel
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -23,11 +29,29 @@ fun CharactersScreen(
     val listCharacters = listOf("1","2","3")
     Scaffold (
         content = { paddingValues ->
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                viewModel.data.value.forEach {character ->
-                    CharacterItem(urlImage = character.image, characterName = character.name){
-                        viewModel.setCharacter(character)
-                        navigateToDetail(it)
+            LazyColumn(
+                Modifier.fillMaxSize(),
+                state = rememberLazyListState()
+            ) {
+                if(viewModel.data.value.isEmpty()) {
+                    items(10) {
+                        GSBCShimmerAnimation() {
+                            Box(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .height(100.dp)
+                                    .padding(horizontal = 20.dp, vertical = 5.dp)
+                                    .background(it)
+                            )
+                        }
+                    }
+                } else {
+                    items(viewModel.data.value) {character ->
+                        CharacterItem(urlImage = character.image, characterName = character.name){
+                            viewModel.setCharacter(character)
+                            navigateToDetail(it)
+                            Toast.makeText(context,it,Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
             }
